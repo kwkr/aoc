7ec858040d7cd54db38c16e1986a3f43d0d60474
kwkr <kawka.maciej.93@gmail.com>
2023-12-04 11:17:18 +0100

    cleanup

--- day2-p2.hs
+++ day2-p2.hs
@@ -9,28 +9,16 @@ trim = f . f
     f = reverse . dropWhile isSpace
 
 extractGames :: String -> [[String]]
-extractGames str = foldr (\x acc -> acc ++ x) [] $ map s $ splitOn ";" $ last $ splitOn ":" str
+extractGames str = foldr ((\x acc -> acc ++ x) . splitElements) [] (splitOn ";" $ last $ splitOn ":" str)
 
-s :: String -> [[String]]
-s str = map words $ map trim $ splitOn "," str
+splitElements :: String -> [[String]]
+splitElements str = map (words . trim) (splitOn "," str)
 
 matchGame :: String -> String
 matchGame str = str
 
 extractIds :: String -> Int
 extractIds str = read $ last $ words $ head $ splitOn ":" str
-
-isValid :: [[String]] -> Bool
-isValid i =
-  foldr
-    ( \(n : r) acc -> case (head r) of
-        "red" -> (read n) <= 12 && acc
-        "green" -> (read n) <= 13 && acc
-        "blue" -> (read n) <= 14 && acc
-        _ -> False
-    )
-    True
-    i
 
 -- limits for the values
 -- 12 red, 13 green, 14 blue
@@ -49,7 +37,6 @@ main = do
   let greenSortedGames = map (filterSortColor "green") g
   let blueSortedGames = map (filterSortColor "blue") g
   let result = zip3 redSortedGames greenSortedGames blueSortedGames
-  print result
   print $
     foldr
       (\(a, b, c) acc -> a * b * c + acc)
